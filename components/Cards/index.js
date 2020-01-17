2ca4c2f2bec4eac3bbb73067c9c447f28bc649ec
Cristina Edens <edens.cristina@gmail.com>
2020-01-17 13:42:20 -0500

    finished sprint MVP met

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,48 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+const cards = document.querySelector("cards-container");
+
+axios
+    .get("https://lambda-times-backend.herokuapp.com/articles")
+    .then(response => {
+        console.log(response);
+        Object.values(response.data.articles).forEach(item => {
+            item.forEach(item => {
+                let cards = document.querySelector(".cards-container");
+                cards.append(articleCreate(item));
+            })
+        })
+    })
+
+.catch(error => {
+    console.log(error);
+})
+
+function articleCreate(article) {
+
+    const card = document.createElement("div");
+    const headline = document.createElement("div");
+    const author = document.createElement("div");
+    const imgContainer = document.createElement("div");
+    const img = document.createElement("img");
+    const span3 = document.createElement("span");
+
+    card.append(headline);
+    card.append(author);
+    card.append(span3);
+    author.append(imgContainer);
+    imgContainer.append(img);
+
+    card.classList.add("card");
+    headline.classList.add("headline");
+    author.classList.add("author");
+    imgContainer.classList.add("img-container");
+
+    img.src = article.authorPhoto;
+    span3.textContent = `By ${article.authorName}`;
+    headline.textContent = article.headline
+
+    return card;
+}
